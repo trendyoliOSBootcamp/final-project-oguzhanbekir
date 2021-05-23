834f4dac6bf678d064e3643b498aecbedd000b1d
Oguzhan Bekir <oguzhanbekir53@gmail.com>
2021-05-23 23:17:43 +0300

    add single view app

---
+++ GameListApp/GameListApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GameListApp
+//
+//  Created by Oguzhan Bekir on 23.05.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
